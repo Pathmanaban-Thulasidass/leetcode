bcf10f511adffdd1d7c71bf66c54cb175241d839
Pathmanaban-Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-07-19 00:23:09 +0530

    [728] [Time Beats: 76.58%] [Memory Beats: 47.11%] - LeetPush

---
+++ 728-Self-Dividing-Numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            if(check(i))
+                ans.add(i);
+        }
+        return ans;
+    }
+    public boolean check(int n){
+        int t = n;
+        while(n>0){
+            int r = n%10;
+            if(r == 0) return false;
+            if(t%r != 0)
+                return false;
+            n /= 10;
+        }
+        return true;
+    }
+}
