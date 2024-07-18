17ee82bf6f833f84670c95dfba09d1460529bc69
Pathmanaban-Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-07-19 00:23:36 +0530

    [728] [Time Beats: 100.00%] [Memory Beats: 25.29%] - LeetPush

--- 728-Self-Dividing-Numbers.java
+++ 728-Self-Dividing-Numbers.java
@@ -9,7 +9,7 @@ class Solution {
     }
     public boolean check(int n){
         int t = n;
-        while(n>0){
+        while(n!=0){
             int r = n%10;
             if(r == 0) return false;
             if(t%r != 0)
