02ac00fa2f08a6f85eb524c71d6e3456d08ed084
Pathmanaban-Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-07-20 10:36:04 +0530

    [645] [Time Beats: 95.40%] [Memory Beats: 47.21%] - LeetPush

--- 645-Set-Mismatch.java
+++ 645-Set-Mismatch.java
@@ -3,18 +3,16 @@ class Solution {
         int n = nums.length;
         int ans[] = new int[2];
         int totalSum = (n*(n+1))/2;
-        int repeatingEle = 0;
         int arrSum = 0;
         boolean found[] = new boolean[n+1];
         for(int i=0;i<n;i++){
             if(found[nums[i]]){
-                repeatingEle = nums[i];
+                ans[0] = nums[i];
             }
             found[nums[i]] = true;
             arrSum += nums[i];
         }
-        ans[0] = repeatingEle;
-        ans[1] = totalSum - (arrSum - repeatingEle);
+        ans[1] = totalSum - (arrSum - ans[0]);
         return ans;
     }
 }
