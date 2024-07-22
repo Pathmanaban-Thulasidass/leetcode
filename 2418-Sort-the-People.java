b13f758314f3ed5087fbce87b6598fc2a025e579
Pathmanaban-Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-07-22 10:46:46 +0530

    [2418] [Time Beats: 94.61%] [Memory Beats: 18.45%] - LeetPush

---
+++ 2418-Sort-the-People.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < heights.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+        Arrays.sort(heights);
+        int ind = 0;
+        for(int i = heights.length - 1; i >= 0; i--){
+            names[ind++] = map.get(heights[i]);
+        }
+        return names;
+    }
+}
