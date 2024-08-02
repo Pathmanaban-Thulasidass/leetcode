f62470a9c6763355cb37464a5b6d3ad60aac023a
Pathmanaban-Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-08-02 18:00:17 +0530

    [205] [Time Beats: 75.27%] [Memory Beats: 16.71%] - LeetPush

--- 205-Isomorphic-Strings.java
+++ 205-Isomorphic-Strings.java
@@ -2,23 +2,13 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length())
             return false;
-        HashMap<Character,Character> map = new HashMap<>();
+        int freq1[] = new int[256];
+        int freq2[] = new int[256];
         for(int i=0;i<s.length();i++){
-            char ori = s.charAt(i);
-            char rep = t.charAt(i);
-            if(!map.containsKey(ori)){
-                if(!map.containsValue(rep)){
-                    map.put(ori,rep);
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                char check = map.get(ori);
-                if(check != rep)
-                    return false;
-            }
+            if(freq1[s.charAt(i)] != freq2[t.charAt(i)])
+                return false;
+            freq1[s.charAt(i)] = i+1;
+            freq2[t.charAt(i)] = i+1;
         }
         return true;
     }
