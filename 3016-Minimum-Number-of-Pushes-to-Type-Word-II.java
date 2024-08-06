458030677c55788f6e62fea3d46b7f2ab8d2734d
Pathmanaban-Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-08-06 21:54:01 +0530

    [3016] [Time Beats: 100.00%] [Memory Beats: 9.23%] - LeetPush

--- 3016-Minimum-Number-of-Pushes-to-Type-Word-II.java
+++ 3016-Minimum-Number-of-Pushes-to-Type-Word-II.java
@@ -1,22 +1,22 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character,Integer> map = new HashMap<>();
+        int freq[] = new int[26];
         int n = word.length();
         int noOfClicked = 0;
         for(int i=0;i<n;i++){
-            map.put(word.charAt(i),map.getOrDefault(word.charAt(i),0) + 1);
+            freq[word.charAt(i) - 'a']++;
         }
-        List<Integer> arr = new ArrayList<>();
-        int j = 0;
+        Arrays.sort(freq);
+        int c = 0;
         int mul = 1;
-        for(char key : map.keySet()){
-            arr.add(map.get(key));
-        }
-        Collections.sort(arr, Collections.reverseOrder());
-        for(int i=0;i<arr.size();i++){
-            noOfClicked += arr.get(i) * mul;
-            if((i+1)%8==0){
-                mul++;
+        for(int i=25;i>=0;i--){
+            if(freq[i] != 0){
+                noOfClicked += freq[i] * mul;
+                c++;
+                if(c == 8){
+                    mul++;
+                    c = 0;
+                }
             }
         }
         return noOfClicked;
