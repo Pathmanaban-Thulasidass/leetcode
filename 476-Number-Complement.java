80cff81b518004ed57b093b89bf627d6026d3a7d
Pathmanaban Thulasidass <153615662+Pathmanaban-Thulasidass@users.noreply.github.com>
2024-08-30 20:25:32 +0530

    [476] [Time Beats: 100.00%] [Memory Beats: 93.86%] - LeetPush

--- 476-Number-Complement.java
+++ 476-Number-Complement.java
@@ -6,7 +6,7 @@ class Solution {
         int i=0;
         while(num>0){
             int reminder=num%2 ^ 1;
-            result+=reminder*Math.pow(2,i++);
+            result+= reminder*Math.pow(2,i++);
             num/=2;
         }
         return result;
