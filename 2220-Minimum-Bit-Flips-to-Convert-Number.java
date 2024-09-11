bcb6cd01b26bf9df30ca1dd9d7d9a9b9a2c2642d
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-09-11 10:45:48 +0530

    [2220] [Time Beats: 100.00%] [Memory Beats: 10.12%] - LeetPush

--- 2220-Minimum-Bit-Flips-to-Convert-Number.java
+++ 2220-Minimum-Bit-Flips-to-Convert-Number.java
@@ -1,23 +1,5 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        StringBuffer startSb = new StringBuffer(Integer.toString(start,2));
-        StringBuffer goalSb = new StringBuffer(Integer.toString(goal,2));
-        int c = 0;
-        if(startSb.length() > goalSb.length()){
-            while(startSb.length() != goalSb.length()){
-                goalSb.insert(0,'0');
-            }
-        }
-        else{
-            while(startSb.length() != goalSb.length()){
-                startSb.insert(0,'0');
-            }
-        }
-        int l = startSb.length();
-        for(int i=0;i<l;i++){
-            if(startSb.charAt(i) != goalSb.charAt(i))
-                c++;
-        }
-        return c;
+        return Integer.bitCount(start ^ goal);
     }
 }
