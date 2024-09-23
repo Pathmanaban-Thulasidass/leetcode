3c498ef4122d7a7d1bac44d39bbdfee0683548b7
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-09-23 17:34:20 +0530

    [78] [Time Beats: 100.00%] [Memory Beats: 6.56%] - LeetPush

--- 78-Subsets.java
+++ 78-Subsets.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
         int size = 1 << n;
-        System.out.println(size);
         List<List<Integer>> ans = new ArrayList<>();
         for(int i=0;i<size;i++){
             List<Integer> inner = new ArrayList<>();
