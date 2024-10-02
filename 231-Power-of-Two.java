9861207fa98d62608f4c6439b352dddafb5cf3ee
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-02 11:14:04 +0530

    [231] [Time Beats: 100.00%] [Memory Beats: 28.00%] - LeetPush

--- 231-Power-of-Two.java
+++ 231-Power-of-Two.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 0 || n == Integer.MIN_VALUE)
-            return false;
-        return ((long)n & (long)(n-1)) == 0;
+        return n > 0 && (n & (n-1)) == 0;
     }
 }
