1f3f9d84afa4d0978caddbf6c3523b2a402a5237
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-03 14:09:36 +0530

    [137] [Time Beats: 100.00%] [Memory Beats: 90.87%] - LeetPush

--- 137-Single-Number-II.java
+++ 137-Single-Number-II.java
@@ -1,19 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
         int n = nums.length;
-        for(int bitIndex = 0;bitIndex < 32;bitIndex++){
-            int count = 0;
-            for(int i=0;i<n;i++){
-                if((nums[i] & (1 << bitIndex)) != 0){
-                    //Set
-                    count++;
-                }
-            }
-            if(count % 3 != 0){
-                ans = ans ^ (1 << bitIndex);
-            }
+        int ones = 0;
+        int twos = 0;
+        for(int i=0;i<n;i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
-        return ans;
+        return ones;
     }
 }
