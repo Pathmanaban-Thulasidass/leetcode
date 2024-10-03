d7f805aba4eaade51d7e063f01b021d1904b7873
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-03 21:21:40 +0530

    [477] [Time Beats: 43.22%] [Memory Beats: 15.63%] - LeetPush

--- 477-Total-Hamming-Distance.java
+++ 477-Total-Hamming-Distance.java
@@ -2,18 +2,14 @@ class Solution {
     public int totalHammingDistance(int[] nums) {
         int hammingDis = 0;
         int setCount = 0;
-        int unSetCount = 0;
         int n = nums.length;
         for(int i=0;i<32;i++){
             setCount = 0;
-            unSetCount = 0;
             for(int j=0;j<n;j++){
                 if(isSet(nums[j],i))
                     setCount++;
-                else
-                    unSetCount++;
             }
-            hammingDis += (setCount * unSetCount);
+            hammingDis += (setCount * (n - setCount));
         }
         return hammingDis;
     }
