a4157d0365c5fae1e130b5b0f0e15005a4d69f84
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-04 23:41:03 +0530

    [1009] [Time Beats: 100.00%] [Memory Beats: 52.17%] - LeetPush

---
+++ 1009-Complement-of-Base-10-Integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int bitwiseComplement(int num) {
+        if(num==0)
+            return 1;
+        int result=0;
+        int i=0;
+        while(num>0){
+            int reminder = (num%2) ^ 1;
+            result+= reminder * Math.pow(2,i++);
+            num/=2;
+        }
+        return result;
+    }
+}
