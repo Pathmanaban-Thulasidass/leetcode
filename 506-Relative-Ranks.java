560e03275694c38f9502793189b7a7570674b549
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-17 23:38:48 +0530

    [506] [Time Beats: 92.18%] [Memory Beats: 90.80%] - LeetPush

--- 506-Relative-Ranks.java
+++ 506-Relative-Ranks.java
@@ -1,26 +1,23 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[1] - a[1]);
         String[] ans = new String[n];
-        Map<Integer,String> map = new HashMap<>();
-        for(int val : score){
-            pq.add(val);
-        }
-        n = pq.size();
         for(int i=0;i<n;i++){
+            pq.add(new int[]{i,score[i]});
+        }
+        int i = 0;
+        while(!pq.isEmpty()){
+            int[] arr = pq.poll();
             if(i == 0)
-                map.put(pq.poll(),\Gold Medal\);
+                ans[arr[0]] = \Gold Medal\;
             else if(i == 1)
-                map.put(pq.poll(),\Silver Medal\);
+                ans[arr[0]] = \Silver Medal\;
             else if(i == 2)
-                map.put(pq.poll(),\Bronze Medal\);
+                ans[arr[0]] = \Bronze Medal\;
             else
-                map.put(pq.poll(),String.valueOf(i + 1));
-        }
-        n = score.length;
-        for(int i=0;i<n;i++){
-            ans[i] = map.get(score[i]);
+                ans[arr[0]] = String.valueOf(i + 1);
+            i++;
         }
         return ans;
     }
