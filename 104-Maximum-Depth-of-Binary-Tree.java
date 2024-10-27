431fbf3dd476cb51b4227b431a67e2484834d5c6
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-27 20:38:36 +0530

    [104] [Time Beats: 100.00%] [Memory Beats: 62.80%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.java
+++ 104-Maximum-Depth-of-Binary-Tree.java
@@ -18,20 +18,8 @@ class Solution {
         if(root == null){
             return 0;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        int depth = 0;
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int noOfNode = queue.size();
-            for(int i=0;i<noOfNode;i++){
-                if(queue.peek().left != null)
-                    queue.add(queue.peek().left);
-                if(queue.peek().right != null)
-                    queue.add(queue.peek().right);
-                queue.poll();
-            }
-            depth++;
-        }
-        return depth;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth) + 1;
     }
 }
