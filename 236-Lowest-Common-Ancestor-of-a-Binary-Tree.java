bc9a7b8d33bebe9bbd99868c0cf16acc0fbd06d1
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-10-28 16:36:54 +0530

    [236] [Time Beats: 21.14%] [Memory Beats: 90.66%] - LeetPush

---
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> ancestorsOfP = new ArrayList<>();
+        List<TreeNode> ancestorsOfQ = new ArrayList<>();
+        findAncestors(root,p,new ArrayList<>(),ancestorsOfP);
+        findAncestors(root,q,new ArrayList<>(),ancestorsOfQ);
+        if(ancestorsOfP.contains(q)){
+            return q;
+        }
+        else if(ancestorsOfQ.contains(p)){
+            return p;
+        }
+        int pPtr = ancestorsOfP.size() - 1;
+        int qPtr = ancestorsOfQ.size() - 1;
+        while(pPtr >= 0 && qPtr >= 0){
+            if(ancestorsOfP.contains(ancestorsOfQ.get(qPtr))){
+                return ancestorsOfQ.get(qPtr);
+            }
+            else if(ancestorsOfQ.contains(ancestorsOfP.get(pPtr))){
+                return ancestorsOfP.get(pPtr);
+            }
+            pPtr--;
+            qPtr--;
+        }
+        return null;
+    }
+    //Pre-order Traversal
+    public void findAncestors(TreeNode root,TreeNode val,List<TreeNode> inner,List<TreeNode> ancestors){
+        if(root == null){
+            return;
+        }
+        if(root == val){
+            ancestors.addAll(inner);
+            return;
+        }
+        inner.add(root);
+        findAncestors(root.left,val,inner,ancestors);
+        findAncestors(root.right,val,inner,ancestors);
+        inner.remove(inner.size() - 1);
+    }
+}
