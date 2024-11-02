f18bf91c0acfcb6ee3bc449ef37b8f5e612327ab
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-11-02 18:14:59 +0530

    [1957] [Time Beats: 88.24%] [Memory Beats: 64.55%] - LeetPush

--- 1957-Delete-Characters-to-Make-Fancy-String.java
+++ 1957-Delete-Characters-to-Make-Fancy-String.java
@@ -1,32 +1,19 @@
 class Solution {
     public String makeFancyString(String s) {
-        int n = s.length();
-        Stack<Character> st = new Stack<>();
         StringBuffer sb = new StringBuffer();
-        for(int i=0;i<n;i++){
-            char ch = s.charAt(i);
-            if(st.size() >= 2){
-                char top1 = st.pop();
-                if(ch == top1){
-                    char top2 = st.pop();
-                    if(ch != top2){
-                        st.push(top2);
-                    }
-                    st.push(top1);
-                    st.push(ch);
-                }
-                else{
-                    st.push(top1);
-                    st.push(ch);
-                }
+        int sameCount = 0;
+        char prev = s.charAt(0);
+        for(char cur : s.toCharArray()){
+            if(prev == cur){
+                sameCount++;
             }
             else{
-                st.push(ch);
+                sameCount = 1;
             }
+            if(sameCount < 3)
+                sb.append(cur);
+            prev = cur;
         }
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
