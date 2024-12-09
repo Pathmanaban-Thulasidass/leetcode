385819dadce13d92fa8f6c250c2baa1411bc40e5
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-09 21:32:33 +0530

    [169] [Time Beats: 78.00%] [Memory Beats: 6.66%] - LeetPush

--- 169-Majority-Element.java
+++ 169-Majority-Element.java
@@ -5,10 +5,9 @@ class Solution {
         int n = nums.length;
         for(int i=0;i<n;i++){
             if(count == 0){
-                count = 1;
                 ele = nums[i];
             }
-            else if(ele == nums[i]){
+            if(ele == nums[i]){
                 count++;
             }
             else if(ele != nums[i]){
