18fb14e1242941ecb99efec72246d076e832384e
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-09 18:27:52 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 7.24%] - LeetPush

---
+++ 27-Remove-Element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        if(n == 0)
+            return 0;
+        int left = 0;
+        int right = n - 1;
+        while(left <= right){
+            while(left < n && nums[left] != val)
+                left++;
+            while(right >= 0 && nums[right] == val)
+                right--;
+            if(left < right){
+                swap(nums,left,right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+    public void swap(int [] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
