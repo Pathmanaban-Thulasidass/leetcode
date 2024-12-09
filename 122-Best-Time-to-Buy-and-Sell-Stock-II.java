75cea4e461e2cb3cf8978f7a6471765a9bbe3c15
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-09 21:53:27 +0530

    [122] [Time Beats: 92.27%] [Memory Beats: 64.18%] - LeetPush

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n-1;i++){
+            if(prices[i] < prices[i+1]){
+                min = Math.min(min,prices[i]);
+            }
+            else{
+                if(min != Integer.MAX_VALUE){
+                    profit += prices[i] - min;
+                }
+                min = Integer.MAX_VALUE;
+            }
+        }
+        if(min != Integer.MAX_VALUE){
+            profit += prices[n-1] - min;
+        }
+        return profit;
+    }
+}
