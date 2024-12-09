790364204ccb4eb8c560c42da60496d33be0579d
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-09 20:43:39 +0530

    [169] [Time Beats: 99.88%] [Memory Beats: 34.08%] - LeetPush

--- 169-Majority-Element.java
+++ 169-Majority-Element.java
@@ -1,6 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int count = 0;
+        int ele = -1;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(count == 0){
+                count = 1;
+                ele = nums[i];
+            }
+            else if(ele == nums[i]){
+                count++;
+            }
+            else if(ele != nums[i]){
+                count--;
+            }
+        }
+        return ele;
     }
 }
