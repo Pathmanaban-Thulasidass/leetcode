c205353da38144e46eec5d5ee100e48e6e2349d7
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-09 18:05:05 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 35.10%] - LeetPush

--- 88-Merge-Sorted-Array.java
+++ 88-Merge-Sorted-Array.java
@@ -1,9 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int j=0;
-        for(int i=m;i<nums1.length;i++){
-            nums1[i] = nums2[j++];
+        m = m + n; 
+        int first = 0;
+        int second = 0;
+        while(first < m && second < n){
+            if(nums1[first] > nums2[second]){
+                moveOneStep(nums1,first,m);
+                nums1[first] = nums2[second];
+                second++;
+            }
+            first++;
         }
-        Arrays.sort(nums1);
+        int temp = m - 1;
+        int tempSec = n - 1;
+        while(tempSec >= second && second < n && temp >= 0){
+            nums1[temp] = nums2[tempSec];
+            temp--;
+            tempSec--;
+        }
+    }
+    public void moveOneStep(int[] nums,int index,int n){
+        for(int i=n-1;i>index;i--){
+           nums[i] = nums[i-1]; 
+        }
     }
 }
