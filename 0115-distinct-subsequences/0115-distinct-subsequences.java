c11db68262d198fd97e75638edc42359673f548a
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-24 14:53:25 +0530

    Time: 18 ms (69.46%), Space: 51.6 MB (15.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -2,24 +2,21 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n1 = s.length();
         int n2 = t.length();
-        return helper(n1-1,n2-1,s,t,new Integer[n1 + 1][n2 + 1]);
-    }
-    public int helper(int i,int j,String s,String t,Integer[][] dp){
-        if(j < 0)
-            return 1;
-        if(i < 0)
-            return  0;
-        if(dp[i][j] != null)
-            return  dp[i][j];
-        int equal = 0;
-        int notEqual = 0;
-        if(s.charAt(i) == t.charAt(j)){
-            equal =  helper(i-1,j-1,s,t,dp) + helper(i-1,j,s,t,dp); 
+
+        int dp[][] = new int[n1 + 1][n2 + 1];
+        for(int i=0;i<=n1;i++){
+            dp[i][0] = 1;
         }
-        else{
-            notEqual = helper(i-1,j,s,t,dp);
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]; 
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        return dp[i][j] = equal + notEqual;
-    }
-    
+        return dp[n1][n2];
+    }    
 }
