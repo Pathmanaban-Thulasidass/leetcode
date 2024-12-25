e7f9f14de16fa57600af7a5f711b633dad20cf36
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2024-12-25 17:58:08 +0530

    Time: 4 ms (91.15%), Space: 45.6 MB (26.25%) - LeetHub

--- 1242-matrix-block-sum/1242-matrix-block-sum.java
+++ 1242-matrix-block-sum/1242-matrix-block-sum.java
@@ -2,22 +2,34 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int m = mat.length;
         int n = mat[0].length;
+        int rowSize = m + (k * 2);
+        int colSize = n + (k * 2);
+        int prefixSum[][] = new int[rowSize][colSize];
         int ans[][] = new int[m][n];
+        for(int i=k;i<rowSize-k;i++){
+            for(int j=k;j<colSize-k;j++){
+                prefixSum[i][j] = mat[i-k][j-k];
+            }
+        }
+        for(int i=k;i<rowSize;i++){
+            int sum = 0;
+            for(int j=k;j<colSize;j++){
+                sum += prefixSum[i][j];
+                prefixSum[i][j] = sum + prefixSum[i-1][j];
+            }
+        }   
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int sum = 0;
-                for(int r=i-k;r<=i+k;r++){
-                    if(r < 0 || r >= m){
-                        continue;
-                    }
-                    for(int c=j-k;c<=j+k;c++){
-                        if(c < 0 || c >= n){
-                            continue;
-                        }
-                        sum += mat[r][c];
-                    }
+                ans[i][j] = prefixSum[i+ 2 * k][j+ 2 * k];
+                if((i+ 2 * k) < rowSize && (j-1) >= 0){
+                    ans[i][j] -= prefixSum[i+ 2 * k][j-1];
                 }
-                ans[i][j] = sum;
+                if((i - 1) >= 0 && (j+ 2 * k) < colSize){
+                    ans[i][j] -= prefixSum[i-1][j+ 2 * k];
+                }
+                if((i-1) >= 0 && (j-1) >= 0){
+                    ans[i][j] += prefixSum[i-1][j-1];
+                }
             }
         }
         return ans;
