2f5b1a616fc1dd1fb7692db85968507feccf6596
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2025-02-22 22:53:19 +0530

    Time: 6 ms (98.02%), Space: 48.1 MB (44.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,7 +2,6 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         int n = strs.length;
         Map<String,List<String>> map = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
         for(int i=0;i<n;i++){
             char[] temp = strs[i].toCharArray();
             Arrays.sort(temp);
@@ -16,9 +15,6 @@ class Solution {
                 map.get(sortedStr).add(strs[i]);
             }
         }
-        for(String key : map.keySet()){
-            ans.add(map.get(key));
-        }
-        return ans;
+        return new ArrayList<>(map.values());
     }
 }
