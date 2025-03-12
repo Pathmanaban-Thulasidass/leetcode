8cf9d1353651c3a5a26aff2eeb4059d5712c1260
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2025-03-12 13:59:53 +0530

    Time: 489 ms (30.32%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with tempTable as(select a1.machine_id, (a2.timestamp - a1.timestamp) as diff 
+    from Activity a1
+    cross join Activity a2
+    where a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a1.activity_type = 'start' and a2.activity_type = 'end')
+
+
+select machine_id, Round((sum(diff) / count(machine_id)),3) as processing_time
+from  tempTable
+group by machine_id;
