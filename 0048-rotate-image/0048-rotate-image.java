73515a0acf070a8738388dde36090e4fde02692a
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2025-05-09 18:41:15 +0530

    Time: 2 ms (3.63%), Space: 42.5 MB (15%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int t;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+        int first = 0;
+        int last = n-1;
+        for(int i=0;i<n;i++){
+            first = 0;
+            last = n-1;
+            while(first < last){
+                t = matrix[i][first];
+                matrix[i][first++] = matrix[i][last];
+                matrix[i][last--] = t;
+            }
+        }
+        
+    }
+}
