f5ee8b4bf4fcccc657cb738b570df040636874d4
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2025-06-02 18:05:19 +0530

    Time: 11 ms (28.79%), Space: 45.6 MB (71.21%) - LeetHub

---
+++ 1647-can-convert-string-in-k-moves/1647-can-convert-string-in-k-moves.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConvertString(String s, String t, int k) {
+        int n1 = s.length();
+        int n2 = t.length();
+        if(n1 != n2)
+            return false;
+        int step[] = new int[26];
+        for(int i=0;i<n1;i++){
+            if(s.charAt(i) != t.charAt(i)){
+                int temp1 = s.charAt(i) - 'a';
+                int temp2 = t.charAt(i) - 'a';
+                int diff = temp2 - temp1;
+                if(diff < 0){
+                    diff = (26 - temp1) + temp2;
+                }
+                if(step[diff] == 0){
+                    step[diff] = diff;
+                }
+                else{
+                    step[diff] += 26;
+                }
+                if(step[diff] > k)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
