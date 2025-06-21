27179bcb3ee508d9a7dc4775a1801eab44404352
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2025-06-21 22:11:55 +0530

    Time: 4 ms (24.11%), Space: 46.5 MB (5.75%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,30 +1,23 @@
 class Solution {
     public int candy(int[] ratings) {
-        //Slope Method
         int n = ratings.length;
-        int totalCandies = 1; // Because first child is even equal to neighbors or greater or lesser it must have 1
-        int i = 1;
-        while(i < n){
-            if(ratings[i] == ratings[i-1]){
-                totalCandies += 1;
-                i++;
-                continue;
+        int ptr = 1;
+        int candy[] = new int[n];
+        Arrays.fill(candy,1);
+        while(ptr < n){
+            if(ratings[ptr] > ratings[ptr - 1]){
+                candy[ptr] = candy[ptr - 1] + 1;
             }
-            int peek = 1;
-            while(i < n && ratings[i] > ratings[i-1]){
-                peek += 1;
-                totalCandies += peek;
-                i++;
+            ptr++;
+        }
+        ptr = n - 2;
+        int totalCandies = candy[n - 1];
+        while(ptr >= 0){
+            if(ratings[ptr] > ratings[ptr + 1]){
+                candy[ptr] = Math.max(candy[ptr],candy[ptr + 1] + 1);
             }
-            int down = 1;
-            while(i < n && ratings[i] < ratings[i-1]){
-                totalCandies += down;
-                down++;
-                i++;
-            }
-            if(down > peek){
-                totalCandies += (down - peek);
-            }
+            totalCandies += candy[ptr];
+            ptr--;
         }
         return totalCandies;
     }
