0e75028c8103445823e9bcadb283c23983806ef8
Pathmanaban Thulasidass <pathmanabanpath02@gmail.com>
2025-07-12 10:04:11 +0530

    Time: 11 ms (89.8%), Space: 45.1 MB (21.93%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
@@ -14,32 +14,7 @@ class Solution {
             else{
                 falseCount++;
             }
-            while(left <= right && trueCount > k){
-                ch = answerKey.charAt(left);
-                if(ch == 'T'){
-                    trueCount--;
-                }
-                else{
-                    falseCount--;
-                }
-                left++;
-            }
-            ans = Math.max(ans,right - left + 1);
-            right++;
-        } 
-        left = 0;
-        right = 0;
-        trueCount = 0;
-        falseCount = 0;
-        while(right < n){
-            char ch = answerKey.charAt(right);
-            if(ch == 'T'){
-                trueCount++;
-            }
-            else{
-                falseCount++;
-            }
-            while(left <= right && falseCount > k){
+            while(Math.min(trueCount,falseCount) > k){
                 ch = answerKey.charAt(left);
                 if(ch == 'T'){
                     trueCount--;
